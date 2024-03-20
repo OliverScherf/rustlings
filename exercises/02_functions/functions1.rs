32d77fb232a94664786835873568d0cfbbc02500
Oliver Scherf <scherf.oliver@gmx.net>
2024-03-20 20:16:18 +0100

    feat: solve some exercises

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,15 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+}
+
+
+fn testSnakeCase() {
+    
+}
+
+fn call_me() {
+    
 }
